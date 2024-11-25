ebb9173601c250761ba8857180f5ffd768ae731d
Ghita Lahbabi <ghlah6@ulaval.ca>
2024-11-24 23:47:40 -0500

    ajout docstrings

--- quixo_error.py
+++ quixo_error.py
@@ -1,5 +1,11 @@
 class QuixoError(Exception):
+    """
+    Classe d'exception personnalisée pour les erreurs liées au jeu Quixo.
+    """
     def __str__(self):
+        """
+        Retourne une représentation lisible de l'exception avec son message.
+        """
         if self.args and self.args[0]:
             f"QuixoError: {self.args[0]}"
         return "QuixoError"
