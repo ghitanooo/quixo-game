7c20fc291aa63ebb240109bc60b2cb102a3d6eb5
Ghita Lahbabi <ghlah6@ulaval.ca>
2024-11-24 23:07:02 -0500

    ajout quixo_error

--- main.py
+++ main.py
@@ -28,4 +28,3 @@ if __name__ == "__main__":
             args.idul,
             SECRET,
         )
-print("test")
--- quixo_error.py
+++ quixo_error.py
@@ -1,3 +1,5 @@
 class QuixoError(Exception):
-
-    pass
+    def __str__(self):
+        if self.args and self.args[0]:
+            f"QuixoError: {self.args[0]}"
+        return "QuixoError"
