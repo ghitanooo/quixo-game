26ec45f9d6565ce0913dbf0d963221dccba3f520
Sarah <salag22@ulaval.ca>
2024-12-18 09:48:43 -0500

    debut classe Quix_ia

--- quixo_ia.py
+++ quixo_ia.py
@@ -1,3 +1,32 @@
-""  ""
-class QuixoIA
+"""Fichier Quixo_ia
+
+Classe:
+    * QuixoIA - Classe utilisée pour jouer automatiquement contre l'ordinateur
+"""
+
+
+class QuixoIA(Quixo):
+    """
+    Fonctions:
+        * lister_les_coups_possibles
+        * analyser_le_plateau
+        * partie_terminée
+        * trouver_un_coup_vainqueur
+        * trouver_un_coup_bloquant
+        * jouer_un_coup
+    """
+    def lister_les_coups_possibles(plateau, symbole):
+        """Lister tous les coups possibles dans une partie
+
+        Args: 
+            * Un plateau de jeu
+            * Symbole du cube représentant le joueur
+
+        Return:
+            *Liste de dictionnaires des coups possibles pour le joueur spécifié
+
+        Raise: 
+            *QuixoError si le cube n'est pas valide, ou si la parties est terminée
+        """
+        
 
